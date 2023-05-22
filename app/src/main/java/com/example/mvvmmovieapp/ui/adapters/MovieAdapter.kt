09b7197727412a4fa1590239934b3bc1ae935426
bosmania2 <45536233+bosmania2@users.noreply.github.com>
2023-05-22 13:34:19 +0300

    Added movie genre display on the trending movies screen

--- app/src/main/java/com/example/mvvmmovieapp/ui/adapters/MovieAdapter.kt
+++ app/src/main/java/com/example/mvvmmovieapp/ui/adapters/MovieAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.mvvmmovieapp.ui.adapters
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
@@ -11,13 +12,14 @@ import com.bumptech.glide.Glide
 import com.example.mvvmmovieapp.R
 import com.example.mvvmmovieapp.apidata.trending.MovieItem
 import com.example.mvvmmovieapp.util.Constants.MOVIE_IMAGE_URL
-import kotlinx.android.synthetic.main.movie_item_view.view.movie_image
-import kotlinx.android.synthetic.main.movie_item_view.view.movie_rating_bar
-import kotlinx.android.synthetic.main.movie_item_view.view.movie_rating_count
-import kotlinx.android.synthetic.main.movie_item_view.view.movie_release_date
-import kotlinx.android.synthetic.main.movie_item_view.view.movie_title
+import kotlinx.android.synthetic.main.movie_item_view.view.*
 
 class MovieAdapter(val context: Context): RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
+
+    private val genreMap = hashMapOf(28 to "Action", 12 to "Adventure", 16 to "Animation", 35 to "Comedy", 80 to "Crime",
+        99 to "Documentary", 18 to "Drama", 10751 to "Family", 14 to "Fantasy", 36 to "History", 27 to "Horror",
+        10402 to "Music", 9648 to "Mystery", 10749 to "Romance", 878 to "Science Fiction", 10770 to "TV Movie",
+        53 to "Thriller", 10752 to "War", 37 to "Western")
 
     inner class MovieViewHolder(view: View): RecyclerView.ViewHolder(view)
 
@@ -40,6 +42,7 @@ class MovieAdapter(val context: Context): RecyclerView.Adapter<MovieAdapter.Movi
             .inflate(R.layout.movie_item_view, parent, false))
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
         val currentMovie = differ.currentList[position]
 
@@ -49,6 +52,7 @@ class MovieAdapter(val context: Context): RecyclerView.Adapter<MovieAdapter.Movi
             movie_title.text = currentMovie.title
             movie_rating_bar.rating = (currentMovie.vote_average / 2).toFloat()
             movie_rating_count.text = currentMovie.vote_count.toString()
+            movie_genre.text = "Genre: " + genreMap.get(currentMovie.genre_ids.first())
             movie_release_date.text = context.resources.getString(R.string.release_date)
                 .format(currentMovie.release_date)
             setOnClickListener {
--- app/src/main/java/com/example/mvvmmovieapp/util/Constants.kt
+++ app/src/main/java/com/example/mvvmmovieapp/util/Constants.kt
@@ -2,7 +2,7 @@ package com.example.mvvmmovieapp.util
 
 object Constants {
     const val MOVIE_API_BASE_URL = "https://api.themoviedb.org/"
-    const val BACKEND_API_BASE_URL = "http://192.168.100.7:8080"
+    const val BACKEND_API_BASE_URL = "http://10.231.3.245:8080"
     const val API_KEY = "9b6dfc91e7f1e6c0dab37b81b9ea33a1"
     const val MOVIE_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
 }
